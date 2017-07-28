f85fc91422b23d8487825ed4dd58b4a2e7e957f0
Jonathan Sosin <jonysosin@gmail.com>
2017-07-28 11:39:06 -0300

    Google Tag Manager Add to Cart Event

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,8 +2,7 @@ var config = {
     map: {
         '*': {
             senderGtm: 'Acs_GoogleTagManager/js/gtm',
-            dataLayer: 'Acs_GoogleTagManager/js/data-layer',
-            addtoCartForm: 'Acs_GoogleTagManager/js/cart-button'
+            dataLayer: 'Acs_GoogleTagManager/js/data-layer'
         }
     },
     deps: [
